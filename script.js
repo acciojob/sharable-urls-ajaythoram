f5fe688c7a9692be366e316133fcc50d11ded00f
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-05-10 07:36:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // your code here
+const txt = document.getElementById("name");
+const num = document.getElementById("number");
+const btn = document.getElementById("buttom");
+const H = document.getElementById("url");
+
+
+
