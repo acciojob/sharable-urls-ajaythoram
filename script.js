84bd8617a331c6c96769a9cd312ca562c69dee37
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-05-10 07:56:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-// your code here
-const txt = document.getElementById("name");
-const num = document.getElementById("number");
-const btn = document.getElementById("buttom");
-const H = document.getElementById("url");
+document.addEventListener('DOMContentLoaded', () => {
+  const txt = document.getElementById("name");
+  const num = document.getElementById("year");
+  const btn = document.getElementById("button");
+  const H = document.getElementById("url");
+
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    H.innerText = `https://localhost:8080/?name=${txt.value}&year=${num.value}`;
+  });
+});
+
 
 
 
